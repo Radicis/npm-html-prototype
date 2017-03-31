a5c102f95c2a582b6ae238d2c0ba35e4bad8ed2c
Adam <alloyddesign@gmail.com>
2017-03-31 21:32:13 +0100

    Added anguar ui layout insted of flexbox began working on scrollbars

--- app/components/app.js
+++ app/components/app.js
@@ -1,3 +1,3 @@
 'use strict';
 
-angular.module('app', []);
+angular.module('app', [require('angular-ui-layout')]);
--- app/components/controllers/homeController.js
+++ app/components/controllers/homeController.js
@@ -1,83 +1,125 @@
 'use strict';
 
 angular.module('app')
+    .controller('HomeCtrl', function($scope){
 
-.controller('HomeCtrl', function($scope){
-    console.log("I am a controller");
+        var fs = require('fs');
+        var path = require('path');
 
-    var fs = require('fs');
-    var path = require('path');
+        var theme = "blackboard";
 
-    var theme = "blackboard";
-
-    $scope.createOutput = function(){
-
-        var output;
-
-        var title = "Output Title";
-
-        var output = '<!DOCTYPE html>\n<html lang="en">\n<head>\n\t<meta charset="UTF-8">\n\t\t<title>' + title +
-            '</title>';
-
-        output += '\n\t<style>\n';
-
-        for(var i=0;i<cssEditor.lineCount();i++){
-            output+= "\t\t" + cssEditor.getLine(i) + "\n";
-        }
-
-        output+= '\n\t</style>\n';
-
-        output += '</head>\n';
-
-        output += '<body>\n';
-
-        for(var h=0;h<htmlEditor.lineCount();h++){
-            output+= "\t" + htmlEditor.getLine(h) + "\n";
-        }
-
-        output += '\n</body>\n';
-
-        output += '<script>\n';
-
-        for(var j=0;j<jsEditor.lineCount();j++){
-            output+= "\t" + jsEditor.getLine(j) + "\n";
-        }
-
-        output+='\n</script>\n</html>';
-
-        fs.writeFile(path.join(__dirname, 'output/output.html'), output, function(err) {
-            if(err) {
-                return console.log(err);
+        var getLines = function(editor){
+            var lines = "";
+            for(var i=0;i<editor.lineCount();i++){
+                lines+= "\t" + editor.getLine(i) + "\n";
             }
+            return lines
+        };
 
-            console.log("The output file was saved!");
+        $scope.layout = {
+            menuPanel: true,
+            codePanel: true,
+            renderPanel: false
+        };
 
-            document.getElementById('output').contentDocument.location.reload(true);
-        });
-    };
+        $scope.toggle = function(which) {
+            $scope.layout[which] = !$scope.layout[which];
+        };
 
-    var htmlEditor = CodeMirror(document.getElementById("code-panel-html"), {
-        mode : "xml",
-        htmlMode: true,
-        lineNumbers: true,
-        scrollbarStyle: "simple",
-        theme: theme,
-        minHeight: 50
+        $scope.close = function(which) {
+            $scope.layout[which] = true;
+        };
+
+        $scope.open = function(which) {
+            $scope.layout[which] = false;
+        };
+
+        var getOutput = function(title){
+            var output = '<!DOCTYPE html>\n<html lang="en">\n<head>\n\t<meta charset="UTF-8">\n\t\t<title>' + title +
+                '</title>';
+
+            output += '\n\t<style>\n';
+
+            output += getLines($scope.cssEditor);
+
+            output+= '\n\t</style>\n';
+
+            output += '</head>\n';
+
+            output += '<body>\n';
+
+            output += getLines($scope.htmlEditor);
+
+            output += '\n</body>\n';
+
+            output += '<script>\n';
+
+            output += getLines($scope.jsEditor);
+
+            output+='\n</script>\n</html>';
+
+            return output;
+        };
+
+        var createOutput = function(){
+
+            var title = "Output Title";
+
+            fs.writeFile(path.join(__dirname, 'output/output.html'), getOutput(title), function(err) {
+                if(err) {
+                    return console.log(err);
+                }
+
+                console.log("The output file was saved!");
+
+                document.getElementById('output').contentDocument.location.reload(true);
+            });
+        };
+
+        $scope.initEditors = function() {
+
+            $scope.htmlEditor = CodeMirror(document.getElementById("code-panel-html"), {
+                mode: "xml",
+                htmlMode: true,
+                lineNumbers: true,
+                scrollbarStyle: "simple",
+                theme: theme,
+                minHeight: 50
+            });
+
+            $scope.cssEditor = CodeMirror(document.getElementById("code-panel-css"), {
+                mode: "css",
+                lineNumbers: true,
+                scrollbarStyle: "simple",
+                theme: theme,
+                minHeight: 50
+            });
+
+            $scope.jsEditor = CodeMirror(document.getElementById("code-panel-js"), {
+                mode: "javascript",
+                lineNumbers: true,
+                scrollbarStyle: "simple",
+                theme: theme,
+                minHeight: 50
+            });
+        }
+
+        // Setup time variables
+        var timer;
+        // time to wait in ms before launching search
+        var time = 1000;
+
+        $scope.initTimer = function(){
+            clearTimeout(timer);
+            timer = setTimeout(doneTyping, time);
+        };
+
+        $scope.clearTimer = function(){
+            clearTimeout(timer);
+        };
+
+        var doneTyping = function () {
+            createOutput();
+        };
+
     });
-
-    var cssEditor = CodeMirror(document.getElementById("code-panel-css"), {
-        mode: "css",
-        lineNumbers: true,
-        scrollbarStyle: "simple",
-        theme: theme,
-        minHeight: 50
-    });
-
-    var jsEditor = CodeMirror(document.getElementById("code-panel-js"), {
-        mode: "javascript",
-        lineNumbers: true,
-        scrollbarStyle: "simple",
-        theme: theme,
-        minHeight: 50
-    });
-});
